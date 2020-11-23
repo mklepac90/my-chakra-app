50b4e9b2d4e411975961ee6f0d52bc54f2181e7d
Michael Klepac <mklepac90@gmail.com>
2020-11-23 14:28:54 -0500

    changed component to utilize chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react"
+
+// const colors = {
+//   brand: {
+//     900: "#1a365d",
+//     800: "#153e75",
+//     700: "#2a69ac",
+//   },
+// }
+
+// const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
 export default MyApp
